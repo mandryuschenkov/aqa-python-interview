9d856172f11e6cfe32aedc73a7a93e7c6862f9a2
Maksim Andryuschenkov <m.andryuschenkov@gmail.com>
2026-03-09 00:32:14 +0300

    fixed xdist token guide

--- pytest-advanced/xdist/shared_state/test_xdist_token.py
+++ pytest-advanced/xdist/shared_state/test_xdist_token.py
@@ -1,7 +1,6 @@
 import os
 import uuid
 import pytest
-from loguru import logger
 
 TOKEN_FILE = "token.txt"
 
@@ -10,12 +9,10 @@ def auth_token():
     if os.path.exists(TOKEN_FILE):
         with open(TOKEN_FILE, "r") as f:
             token = f.read()
-            logger.debug(f"Token exists: {token=}")
             if token:
                 return token
             
     token = str(uuid.uuid4())
-    logger.debug(f"New token: {token=}")
     with open(TOKEN_FILE, "w") as f:
         f.write(token)
         
